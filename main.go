ec41b6337a52babe6fbaf55af2799c69d23f7eb7
Keonik1 <keonik.dev@gmail.com>
2025-05-08 12:51:24 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	session, err := discordgo.New("Bot 1234sdfg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, m.Content)
+	})
+
+	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = session.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	log.Print("The Bot is online")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
