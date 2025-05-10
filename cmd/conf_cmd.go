2664b44a798314171c89af3e787d47ef16536b6d
Keonik1 <keonik.dev@gmail.com>
2025-05-10 17:21:28 +0300

    move commands to own package
    - move example commands to own file
    - add for every command type own file

---
+++ cmd/conf_cmd.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "fmt"
+
+func HelloConfCommand() {
+	fmt.Println("Hello from conf cmd!")
+}
---
+++ cmd/mod_cmd.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "fmt"
+
+func HelloModCommand() {
+	fmt.Println("Hello from mod cmd!")
+}
